035df3f46309364cb88e0155f7f8862a0db67b0a
Yanis Touaitia <touaitiay1@roverkids.org>
2025-05-16 14:19:06 -0400

    update

--- UserInterface/User Interface/Assets/Course Library/Scripts/GameManager.cs
+++ UserInterface/User Interface/Assets/Course Library/Scripts/GameManager.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    
     public GameObject[] targetPrefabs;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -20,6 +26,12 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targetPrefabs.Count());
             Instantiate(targetPrefabs[index]);
+            UpdateScore(5);
         }
+    }
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
